e165642e250380257e91860a6990b64c81d040b3
Gabriel DM <gnizto@outlook.com>
2022-11-28 21:30:11 -0300

    refactor: add aria for accessibility

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,10 +11,27 @@ window.onload = () => {
 
   // script for toggle menu on mobile
   const btnMenuMobile = document.getElementById('btn-menu-mobile')
-
-  btnMenuMobile.addEventListener('click', () => {
+  
+  
+  const toggleMenu = (event) => {
+    
+    if (event.type === "touchstart") event.preventDefault();
+    
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
-  })
+
+    const active = nav.classList.contains('active')
+
+    event.currentTarget.setAttribute('aria-expanded', active)
+
+    if (active) {
+      event.currentTarget.setAttribute('aria-label', 'Close menu')
+    } else {
+      event.currentTarget.setAttribute('aria-label', 'Open menu')
+    }
+  }
+  
+  btnMenuMobile.addEventListener('click', toggleMenu)
+  btnMenuMobile.addEventListener('touchstart', toggleMenu)
 }
 
